63c7fac3c459a1ad7d20e0a902df936419a0e4cd
RasmusLynge <RasmuslJacobsen@live.com>
2020-03-05 09:57:58 +0100

    firsty

---
+++ src/main/java/org/acme/keycloak/authorization/AdminResource.java
@@ -0,0 +1,18 @@
+package org.acme.keycloak.authorization;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/admin")
+public class AdminResource {
+
+    @GET
+    @RolesAllowed("admin")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String manage() {
+        return "granted";
+    }
+}
